73f6b9b482bb2eace1895aa68bbb962f653e4d09
YIN <yin@YINs-MacBook-Pro.local>
2020-04-02 15:28:30 +0200

    add a0

--- vasp_python/yin_vasp_CONTCAR_to_Cartesian.py
+++ vasp_python/yin_vasp_CONTCAR_to_Cartesian.py
@@ -3,26 +3,49 @@
 import numpy as np
 from ase.io.vasp import read_vasp, write_vasp
 from ase.constraints import FixedLine
-
-import sys
-# Count the arguments
-narg = len(sys.argv) - 1
+import sys, os
 
 
-ASE_Atoms = read_vasp('./CONTCAR')
+filename1 = './CONTCAR'
+
+
+f = open(filename1,'r')
+a0 = float( f.readlines()[1] )
+f.close()
+
+ASE_Atoms = read_vasp(filename1)
 atoms_pos = ASE_Atoms.get_positions()
 natoms = atoms_pos.shape[0]
 
+ASE_Atoms.set_positions( atoms_pos/a0   )
+ASE_Atoms.set_cell( ASE_Atoms.cell/a0   )
 
-if narg == 1:
+
+if len(sys.argv)  == 1:        
+    filename2 = 'POSCAR_Cartesian'
+
+elif len(sys.argv)  == 2:
     if sys.argv[1] == '-FFT':
+        filename2 = 'POSCAR_Cartesian_FFT'
         constrained_atoms = np.arange(natoms)  # apply constraint to all atoms
-        constraint_direction = ASE_Atoms.cell[2,:]       # relax only in the a3 direction
+        constraint_direction = ASE_Atoms.cell[2,:]          # relax only in the a3 direction
         zonly_constraint = FixedLine(constrained_atoms, constraint_direction)
         ASE_Atoms.set_constraint(zonly_constraint)
 
 
-write_vasp('POSCAR_Cartesian', ASE_Atoms,
-label='system_name')
+write_vasp(filename2, ASE_Atoms,
+label='system_name', direct=False)
+
+
+with open(filename2) as f:
+    lines = f.readlines()
+
+lines[1] = ' %.16f \n' % (a0)
+
+with open('file_temp', "w") as f:
+    f.writelines(lines)
+
+os.replace('file_temp', filename2)
+
 
 
